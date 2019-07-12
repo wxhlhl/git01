f86ac8689f31e177ec330d11d69a5649d4a7ce17
wxh <11@11.com>
2019-07-12 17:45:13 +0800

    第二次提交

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,5 +1,13 @@
 public class User {
     private  String name;
     private  String address1;
+    private String phone;
+
+    public User(String name, String address1, String phone) {
+        this.name = name;
+        this.address1 = address1;
+        this.phone = phone;
+    }
+}
     private String phone3;
   }
